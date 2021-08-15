ecf755a248618d106f50c94c10829942ebc76ff1
Ryan W <rwilliamz@gmail.com>
2021-08-14 21:31:31 -0700

    Fix install script

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ ln -s ~/dev/dotfiles/zsh/.zprofile ~/.zprofile
 ln -s ~/dev/dotfiles/zsh/.zshrc ~/.zshrc
 
 # create vim/neovim symlinks
-ln -s ~/dev/dotfiles/config/nvim/init.vm ~/.config/nvim/init.vm
+ln -s ~/dev/dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
