6075b83ab161d8ac7423051c7c8cff4b0cf2010b
Ryan W <rwilliamz@gmail.com>
2021-08-14 20:25:21 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# create zsh symlinks
+ln -s ~/dev/dotfiles/zsh/.zprofile ~/.zprofile
+ln -s ~/dev/dotfiles/zsh/.zshrc ~/.zshrc
+
+# create vim/neovim symlinks
+ln -s ~/dev/dotfiles/config/nvim/init.vm ~/.config/nvim/init.vm
