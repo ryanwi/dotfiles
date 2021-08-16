7e89cc7ae74a0957a68502331ebdc70971ea282d
Ryan W <rwilliamz@gmail.com>
2021-08-16 15:09:33 -0700

    install vim plugin

--- install.sh
+++ install.sh
@@ -4,5 +4,13 @@
 ln -s ~/dev/dotfiles/zsh/.zprofile ~/.zprofile
 ln -s ~/dev/dotfiles/zsh/.zshrc ~/.zshrc
 
-# create vim/neovim symlinks
+# create neovim symlinks
+mkdir ./config/nvim
+mkdir ./config/nvim/autoload
+mkdir ./config/nvim/plugged
+
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -s ~/dev/dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
+
